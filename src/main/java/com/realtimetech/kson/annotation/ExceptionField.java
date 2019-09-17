ab7945bac248e77be1d309387589bae3dd15d8e0
StaticDefault <dev.parkjeonghwan@gmail.com>
2019-09-17 12:03:34 +0900

    Fixed errors, added exception field/byte character feature

--- src/main/java/com/realtimetech/kson/annotation/ExceptionField.java
+++ src/main/java/com/realtimetech/kson/annotation/ExceptionField.java
@@ -1,9 +1,9 @@
-package com.realtimetech.kson.primary;
+package com.realtimetech.kson.annotation;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface PrimaryKey {
+public @interface ExceptionField {
 
 }
